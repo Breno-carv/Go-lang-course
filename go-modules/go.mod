32e5e7444645b63145c848f8dec8b362105560db
BVC <carv.breno.dev@gmail.com>
2026-04-28 10:46:23 -0300

    add: mystrings local pkg and replace methods to local dev

--- go-modules/go.mod
+++ go-modules/go.mod
@@ -1,3 +1,9 @@
 module github.com/Breno-carv/go-modules
 
 go 1.24.4
+
+replace github.com/Breno-carv/mystrings v0.0.0 => ../mystrings
+
+require (
+github.com/Breno-carv/mystrings v0.0.0
+)
---
+++ go-modules/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Breno-carv/mystrings"
+)
+
+func main() {
+	fmt.Println("hello modules!")
+	fmt.Println(mystrings.Reverse("Hello world!"))
+}
